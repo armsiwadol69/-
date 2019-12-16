9bec5373fc35ae4a17e95af4f1a66208985b72a6
armsiwadol69 <30834570+armsiwadol69@users.noreply.github.com>
2019-12-16 21:26:15 +0700

    Add files via upload

---
+++ lab2_boolean.py
@@ -0,0 +1,209 @@
+'''Lab 2 Boolean / Siwadol Malisorn DM6201 No.12 116210806017-6'''
+'''Boolean Expressions and Operators'''
+#No.1 2 < 3 #True
+print(2<3)
+a1 = 2
+b1 = 3
+if a1<b1 :
+    print("TRUE")
+else:print("Flase")
+#No.1 EX
+print(69<70)
+exa1 = 69
+exb2 = 70
+if exa1<exb2 :
+    print("TRUE")
+else:
+    print("Flase")
+#No.2 2 < 3 #True
+print(3<2)
+a2 = 3
+b2 = 2
+if a2<b2 :
+    print("TRUE")
+else:print("Flase")
+#No.2 EX
+print(55<44)
+exa2 = 55
+exb2 = 44
+if exa2<exb2 :
+    print("TRUE")
+else:
+    print("Flase")
+#No.3 5-1 > 2+1
+print(5-1>2+1)
+a3=5-1
+b3=2+1
+if a3>b3 :
+    print("TRUE")
+else:
+    print("False")
+#No.3 EX
+print(10-3>5-3)
+exa3=5-1
+exb3=2+1
+if exa3>exb3 :
+    print("TRUE")
+else:
+    print("False")
+#No.4 3+5==4+4
+print(3+5==4+4)
+a4=3+5
+b4=4+4
+if a4==b4 :
+    print("True")
+else:
+    print("Flase")
+#No.4 EX
+print(10+10==15+5)
+exa4=10+10
+exb4=15+5
+if exa4==exb4 :
+    print("True")
+else:
+    print("Flase")
+#No.5 3 = 5-3
+print(3 == 5-3)
+a5=3
+b5=5-3
+if a5==b5 :
+    print("True")
+else:
+    print("Flase")
+#No.5 EX
+print(10==15-6)
+exa5=10
+exb5=15-6
+if exa5==exb5 :
+    print("True")
+else:
+    print("Flase")
+#No.6 3 != 4
+print(3 != 4)
+a6=3
+b6=4
+if a6 != b6 :
+    print("True")
+else:
+    print("False")
+#No.6 EX
+print(10 != 10.5)
+exa6=10
+exb6=10.5
+if exa6 != exb6 :
+    print("True")
+else:
+    print("False")
+#No.7 3 >= 4
+print(3 >= 4)
+a7=3
+b7=4
+if a7 >= b7 :
+    print("True")
+else:
+    print("False")
+#No.7 EX
+print(8 >= 9)
+exa7=8
+exb7=9
+if exa7 >= exb7 :
+    print("True")
+else:
+    print("False")
+#No.8 2<3 and 4>5
+print(2<3 and 4>5)
+a8 = 2<3
+b8 = 4>5
+if a8 and b8 :
+    print("True")
+else:
+    print("Fasle")
+#No.8 EX
+print(5<6 and 8>10)
+exa8 = 5<6
+exb8 = 8>10
+if exa8 and exb8 :
+    print("True")
+else:
+    print("Fasle")
+#No.9 2<3 and True
+print(2<3 and True)
+a9=2<3
+if a9 and True :
+    print("True")
+else:
+    print("False")
+#No.9 EX
+print(50<51 and True)
+exa9= 50<51
+if exa9 and True :
+    print("True")
+else:
+    print("False")
+#No.10 3<4 or 4<3
+print(3<4 or 4<3)
+a10=3<4
+b10=4<3
+if a10 or b10:
+    print("True")
+else:
+    print("False")
+#No.10 EX
+print(6<7 or 1<2)
+exa10=6<7
+exb10=1<2
+if exa10 or exb10:
+    print("True")
+else:
+    print("False")
+#No.11
+print(3<2 or 2<1)
+a11=3<2
+b11=2<1
+if a11 or b11:
+    print("True")
+else:
+    print("False")
+#No.11 EX
+print(5<4 or 3<2)
+exa11=5<4
+exb11=3<2
+if exa11 or exb11:
+    print("True")
+else:
+    print("False")
+#No.12 print(bool("Hello"))
+print(bool("Hello"))
+#EX
+print(bool("IwantToSleep"))
+#No.13 print(bool(15))
+print(bool(15))
+#EX
+print(bool(69))
+#No.14
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+#No.14 EX
+x189 = "Sleep"
+y190 = "51"
+print(bool(x189))
+print(bool(y190))
+#No.15
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+#No.16 Functions can Return a Boolean
+x199 = 200
+print(isinstance(x189,int))
+#No.16 EX
+y199 = 199
+print(isinstance(y199,int))
+
+
+
+
+
+
