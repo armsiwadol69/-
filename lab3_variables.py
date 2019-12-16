790992ffc7b1876ada683450f978215e33e4926d
armsiwadol69 <30834570+armsiwadol69@users.noreply.github.com>
2019-12-16 21:29:44 +0700

    Add files via upload

---
+++ lab3_variables.py
@@ -0,0 +1,31 @@
+#Siwadol Malisorn DM6201 No.12 116210806017-6
+'''#No.1 operator is the name of the variable and the operand to the right of the = operator is the value
+stored in the variable.'''
+#No.1
+x=4
+print(x)
+#No.1 EX
+y=69
+print(y)
+'''Multi-Line Statements'''
+item_one = 50 #Integer
+item_two = 58.8 # Floating point
+item_three = "Python is EASY " #String
+item_four = "Very Much" #String
+total = item_one ,\
+        item_two, \
+        item_three +\
+        item_four
+print(total)
+'''EX Multi-Line Statements'''
+item_one = 44 #Integer
+item_two = 1.12 # Floating point
+item_three = "3 Lab in One Day " #String
+item_four = "Send Before 00:00PM " #String
+item_five = "OMG"
+total2 = item_one ,\
+        item_two, \
+        item_three +\
+        item_four +\
+        item_five
+print(total2)
